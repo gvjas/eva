34ce6f958f934d36c28348c0fe4dd88a9d759ea4
gvjas <gvjasch@gmail.com>
2022-05-02 08:30:48 +0300

    feat: add KNeighborsClassifier and cross validate kfold

--- evaluation_selection/train.py
+++ evaluation_selection/train.py
@@ -2,9 +2,11 @@ from pathlib import Path
 
 import click
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, f1_score, jaccard_score
+from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
 
 @click.command()
 @click.option(
@@ -19,8 +21,11 @@ def train(dataset_path: Path) -> None:
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
     features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.2, random_state=42
+        features, target, test_size=0.1, random_state=42
     )
-    classifier = LogisticRegression(random_state=42).fit(features_train, target_train)
-    accuracy = accuracy_score(target_val, classifier.predict(features_val))
 
+    classifier = KNeighborsClassifier(n_jobs=-1).fit(features_train, target_train)
+
+    target_pred = classifier.predict(features_val)
+    scores = cross_validate(classifier, features_train, target_train, cv=KFold(3))
+    click.echo(f"Scores: {scores}\n")
