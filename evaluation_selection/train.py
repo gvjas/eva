91c43d5d68a915bd89b2294c72b9a67d2c44fa1b
gvjas <gvjasch@gmail.com>
2022-05-02 09:17:37 +0300

    feat: add CLI option kfold

--- evaluation_selection/train.py
+++ evaluation_selection/train.py
@@ -23,7 +23,9 @@ from sklearn.model_selection import cross_validate
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
 )
 
-def train(dataset_path: Path, random_state: int, test_size: float) -> None:
+@click.option("--kfold", default=5, type=int)
+
+def train(dataset_path: Path, random_state: int, test_size: float, kfold: int) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
@@ -36,7 +38,7 @@ def train(dataset_path: Path, random_state: int, test_size: float) -> None:
 
     target_pred = classifier.predict(features_val)
     scoring = ['accuracy', 'f1_macro', 'jaccard_macro']
-    scores = cross_validate(classifier, features_train, target_train, cv=KFold(3), scoring=scoring)
+    scores = cross_validate(classifier, features_train, target_train, cv=KFold(kfold), scoring=scoring)
     click.echo(f"Scores: {scores}\n"
                f"Accuracy: {scores['test_accuracy'].mean()}, "
                f"f1 score: {scores['test_f1_macro'].mean()}, "
