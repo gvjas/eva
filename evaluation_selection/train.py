087f8505ee13cf4a1e5acafbda1d72073fa3feaf
gvjas <gvjasch@gmail.com>
2022-05-02 09:06:06 +0300

    feat: add CLI option test size

--- evaluation_selection/train.py
+++ evaluation_selection/train.py
@@ -17,13 +17,19 @@ from sklearn.model_selection import cross_validate
 )
 @click.option("--random-state", default=42, type=int)
 
-def train(dataset_path: Path, random_state: int) -> None:
+@click.option(
+    "--test-size",
+    default=0.1,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+)
+
+def train(dataset_path: Path, random_state: int, test_size: float) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
     features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.1, random_state=random_state
+        features, target, test_size=test_size, random_state=random_state
     )
 
     classifier = KNeighborsClassifier(n_jobs=-1).fit(features_train, target_train)
