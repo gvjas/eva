cab2919e7999821650f95d59aef87e0ffb27fc52
gvjas <gvjasch@gmail.com>
2022-05-02 08:57:40 +0300

    feat: add CLI option random state

--- evaluation_selection/train.py
+++ evaluation_selection/train.py
@@ -15,13 +15,15 @@ from sklearn.model_selection import cross_validate
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
-def train(dataset_path: Path) -> None:
+@click.option("--random-state", default=42, type=int)
+
+def train(dataset_path: Path, random_state: int) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
     features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.1, random_state=42
+        features, target, test_size=0.1, random_state=random_state
     )
 
     classifier = KNeighborsClassifier(n_jobs=-1).fit(features_train, target_train)
